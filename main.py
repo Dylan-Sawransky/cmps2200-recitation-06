b6bb9edde4b2a1ac6111c0d525302bf3912f4985
Dylan-Sawransky <Dsawransky@icloud.com>
2025-10-28 14:59:51 -0500

    Implement Fibonacci functions in main.py
    Implemented Fibonacci functions: recursive, top-down, and bottom-up.

--- main.py
+++ main.py
@@ -5,20 +5,46 @@ def fib_recursive(n, counts):
     each time fib_recursive(i, counts) is called.
     """    
     counts[n] += 1
-    ###TODO
-    pass
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    return fib_recursive(n-1, counts) + fib_recursive(n-2, counts)
     
 
-    
 def fib_top_down(n, fibs):
-    ###TODO
-    pass
+
+    if fibs[n] != -1:
+        return fibs[n]
+
+    if n == 0:
+        fibs[0] = 0
+        return 0
+    if n == 1:
+        fibs[1] = 1
+        return 1
+
+    fibs[n] = fib_top_down(n-1, fibs) + fib_top_down(n-2, fibs)
+    return fibs[n]
 
 
 def fib_bottom_up(n):
-    ###TODO
-    pass
 
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    table = [0] * (n+1)
+    table[0] = 0
+    table[1] = 1
+
+    for i in range(2, n+1):
+        table[i] = table[i-1] + table[i-2]
+
+    return table[n]
 
 
 
